a75374072a53203de6b5787f866e72f9037f0208
Hamidreza <hamidreza004@gmail.com>
2021-02-24 18:33:57 +0330

    Complete script.sh to download video and audio and merget them

--- script.sh
+++ script.sh
@@ -8,9 +8,9 @@ cd $name
 
 download(){
 	while [ 1 ]; do
-		wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 --continue $1
+		wget --retry-connrefused --waitretry=0.1 --read-timeout=20 --timeout=15 -t 0 --continue $1
 	    if [ $? = 0 ]; then break; fi; # check return value, break if successful (0)
-		sleep 1s;
+		sleep 0.1
 	done;
 }
 
@@ -20,9 +20,21 @@ download "$link/index-f5-v1.m3u8"
 download "$link/index-f1-a1.m3u8"
 
 segments=`cat index-f5-v1.m3u8 | wc -l`
-segments=`echo $((segments/2-5))`
+segments=`echo $((segments/2-4))`
 
-#for i in {1..50}; do
-#    echo "$link/seg-$i-f5-v1.ts"
-#done
-echo $segments
+for ((i=1;i<=$segments;i++)); do
+    download "$link/seg-$i-f5-v1.ts"
+done
+
+for ((i=1;i<=$segments;i++)); do
+	download "$link/seg-$i-f1-a1.ts"
+done
+
+cp ../master.m3u8 .
+python3 -m http.server &
+sleep 5
+youtube-dl --hls-use-mpegts --verbos --retries "infinite" --fragment-retries "infinite" "http://localhost:8000/master.m3u8"
+
+rm *.ts
+rm *.m3u8
+rm *.key
